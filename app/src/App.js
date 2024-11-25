4c5c9c615e6a7af99213475bcb73e9e7bfa7e97e
JeffreyArrosio <jeffrey.aj98@gmail.com>
2024-11-25 21:28:42 +0000

    Turbo mamag evo

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import React from "react";
-import { useSpeech } from "react-text-to-speech";
+import React, { useState } from 'react';
 
 export default function App() {
-  const {
-    Text, // Component that returns the modified text property
-    speechStatus, // String that stores current speech status
-    isInQueue, // Boolean that stores whether a speech utterance is either being spoken or present in queue
-    start, // Function to start the speech or put it in queue
-    pause, // Function to pause the speech
-    stop, // Function to stop the speech or remove it from queue
-  } = useSpeech({ text: "mamagu evo" });
+  const text = "Turbo mamagu evo";
+
+  const handleSpeak = () => {
+    if (text.trim() !== '') {
+      const utterance = new SpeechSynthesisUtterance(text);
+      window.speechSynthesis.speak(utterance);
+    }
+  };
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", rowGap: "1rem" }}>
-      <Text />
-      <div style={{ display: "flex", columnGap: "0.5rem" }}>
-        {speechStatus !== "started" ? <button onClick={start}>Start</button> : <button onClick={pause}>Pause</button>}
-        <button onClick={stop}>Stop</button>
-      </div>
+    <div>
+      <h1>Texto a Voz</h1>
+      <div
+      onMouseOver={handleSpeak}
+      >{text}</div>
     </div>
   );
 }
